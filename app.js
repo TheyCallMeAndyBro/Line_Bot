6758a9c886c7f017e2f786cb7c5b3b6cd4221746
TheyCallMeAndyBro <andy060104@gmail.com>
2023-11-17 17:51:03 +0800

    initialize project

---
+++ app.js
@@ -0,0 +1,58 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 3000;
+
+
+
+
+require('dotenv').config();
+
+const line = require('@line/bot-sdk');
+const express = require('express');
+
+// create LINE SDK config from env variables
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+
+// create LINE SDK client
+const client = new line.messagingApi.MessagingApiClient({
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+});
+
+
+
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+// event handler
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+
+  // create a echoing text message
+  const echo = { type: 'text', text: event.message.text };
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
+
+
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
